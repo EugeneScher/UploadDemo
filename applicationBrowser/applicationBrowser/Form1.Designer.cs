3a8ea7bed1ad3bff86502539b5abb04ce0175361
EugeneScher <sher3456moon@gmail.com>
2024-06-12 17:17:44 +0700

    upload

---
+++ applicationBrowser/applicationBrowser/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace applicationBrowser
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.webBrowser = new System.Windows.Forms.WebBrowser();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.buttonHome = new System.Windows.Forms.Button();
+            this.buttonSearch = new System.Windows.Forms.Button();
+            this.buttonRefresh = new System.Windows.Forms.Button();
+            this.buttonStop = new System.Windows.Forms.Button();
+            this.textBoxAdress = new System.Windows.Forms.TextBox();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // webBrowser
+            // 
+            this.webBrowser.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webBrowser.Location = new System.Drawing.Point(0, 0);
+            this.webBrowser.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser.Name = "webBrowser";
+            this.webBrowser.ScriptErrorsSuppressed = true;
+            this.webBrowser.Size = new System.Drawing.Size(484, 361);
+            this.webBrowser.TabIndex = 0;
+            this.webBrowser.Url = new System.Uri("http://google.com", System.UriKind.Absolute);
+            this.webBrowser.DocumentCompleted += new System.Windows.Forms.WebBrowserDocumentCompletedEventHandler(this.webBrowser_DocumentCompleted);
+            this.webBrowser.Navigating += new System.Windows.Forms.WebBrowserNavigatingEventHandler(this.webBrowser_Navigating);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.textBoxAdress);
+            this.panel1.Controls.Add(this.buttonStop);
+            this.panel1.Controls.Add(this.buttonRefresh);
+            this.panel1.Controls.Add(this.buttonSearch);
+            this.panel1.Controls.Add(this.buttonHome);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(484, 53);
+            this.panel1.TabIndex = 1;
+            // 
+            // buttonHome
+            // 
+            this.buttonHome.Location = new System.Drawing.Point(3, 3);
+            this.buttonHome.Name = "buttonHome";
+            this.buttonHome.Size = new System.Drawing.Size(50, 23);
+            this.buttonHome.TabIndex = 0;
+            this.buttonHome.Text = "HOME";
+            this.buttonHome.UseVisualStyleBackColor = true;
+            this.buttonHome.Click += new System.EventHandler(this.buttonHome_Click);
+            // 
+            // buttonSearch
+            // 
+            this.buttonSearch.Location = new System.Drawing.Point(59, 3);
+            this.buttonSearch.Name = "buttonSearch";
+            this.buttonSearch.Size = new System.Drawing.Size(59, 23);
+            this.buttonSearch.TabIndex = 1;
+            this.buttonSearch.Text = "SEARCH";
+            this.buttonSearch.UseVisualStyleBackColor = true;
+            this.buttonSearch.Click += new System.EventHandler(this.buttonSearch_Click);
+            // 
+            // buttonRefresh
+            // 
+            this.buttonRefresh.Location = new System.Drawing.Point(124, 3);
+            this.buttonRefresh.Name = "buttonRefresh";
+            this.buttonRefresh.Size = new System.Drawing.Size(67, 23);
+            this.buttonRefresh.TabIndex = 2;
+            this.buttonRefresh.Text = "REFRESH";
+            this.buttonRefresh.UseVisualStyleBackColor = true;
+            this.buttonRefresh.Click += new System.EventHandler(this.buttonRefresh_Click);
+            // 
+            // buttonStop
+            // 
+            this.buttonStop.Location = new System.Drawing.Point(197, 3);
+            this.buttonStop.Name = "buttonStop";
+            this.buttonStop.Size = new System.Drawing.Size(45, 23);
+            this.buttonStop.TabIndex = 3;
+            this.buttonStop.Text = "STOP";
+            this.buttonStop.UseVisualStyleBackColor = true;
+            this.buttonStop.Click += new System.EventHandler(this.buttonStop_Click);
+            // 
+            // textBoxAdress
+            // 
+            this.textBoxAdress.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.textBoxAdress.Location = new System.Drawing.Point(0, 33);
+            this.textBoxAdress.Name = "textBoxAdress";
+            this.textBoxAdress.Size = new System.Drawing.Size(484, 20);
+            this.textBoxAdress.TabIndex = 4;
+            this.textBoxAdress.Text = "http://google.com";
+            this.textBoxAdress.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBoxAdress_KeyPress);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(484, 361);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.webBrowser);
+            this.Name = "Form1";
+            this.Text = "browser";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.WebBrowser webBrowser;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button buttonStop;
+        private System.Windows.Forms.Button buttonRefresh;
+        private System.Windows.Forms.Button buttonSearch;
+        private System.Windows.Forms.Button buttonHome;
+        private System.Windows.Forms.TextBox textBoxAdress;
+    }
+}
+
---
+++ applicationBrowser/applicationBrowser/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace applicationBrowser
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void webBrowser_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            buttonStop.Enabled = false;
+        }
+
+        private void textBoxAdress_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)13) webBrowser.Navigate(textBoxAdress.Text);
+        }
+
+        private void buttonSearch_Click(object sender, EventArgs e)
+        {
+            string url = textBoxAdress.Text;
+            if (!url.StartsWith("http://") && !url.StartsWith("https://")) url = "http://" + url;
+            webBrowser.Navigate(new Uri(url));
+        }
+
+        private void buttonHome_Click(object sender, EventArgs e)
+        {
+            webBrowser.Navigate(new Uri("http://google.com"));
+        }
+
+        private void buttonRefresh_Click(object sender, EventArgs e)
+        {
+            webBrowser.Refresh();
+        }
+
+        private void buttonStop_Click(object sender, EventArgs e)
+        {
+            webBrowser.Stop();
+        }
+
+        private void webBrowser_Navigating(object sender, WebBrowserNavigatingEventArgs e)
+        {
+            buttonStop.Enabled = true;
+        }
+    }
+}
